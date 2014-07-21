93fb461c9677fd6885ef4f27b54a062cad897919
Sean Ardley <sean.ardley@gmail.com>
2014-07-21 16:54:42 -0700

    Tons of changes.  httpPOST is now done from a broadcast receiver sent by the original broadcast receiver.

--- src/com/example/superpushv1/ToastMakerBR.java
+++ src/com/example/superpushv1/ToastMakerBR.java
@@ -5,17 +5,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+
+
 public class ToastMakerBR extends BroadcastReceiver{
 
+	
+	private Context lastActivityContext; //The context of the activity that called this in receiver in the first place)
+	private CharSequence intentData;
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
 
 		// Extract data included in the Intent
-		CharSequence intentData = intent.getCharSequenceExtra("message");	
+		intentData = intent.getCharSequenceExtra("message");
+		lastActivityContext = context;
 		Toast.makeText(context, "ToastMakerBR received the Intent's message: "+intentData, Toast.LENGTH_LONG).show();
+		broadcastHttpPostIntent();
+	}
+	
+	private void broadcastHttpPostIntent(){ 					//Sends intent
+	       Intent intent = new Intent("HttpPostSenderIntent");
+	       intent.putExtra("message", intentData);
+	       intent.setAction("com.example.superpushV1.HTTPPOSTSENDER_INTENT");
+	       lastActivityContext.sendBroadcast(intent);
 	}
 
+	
 }
 
